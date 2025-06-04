3370d5f9b49a7220d332c4bd70556f8d56154087
ErwinHlz <erwinholzhauser.eh@gmail.com>
2025-06-04 16:51:01 +0200

    added quiz

--- FrontEnd/src/components/Quiz.vue
+++ FrontEnd/src/components/Quiz.vue
@@ -1,30 +1,97 @@
 <template>
+  <head>
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap"
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&family=Tiny5&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+
   <div class="quiz-container">
-    <a href="/LoeschZwerge/quizbrandschutz">Brandschutz-Quiz</a>
-    <a href="/LoeschZwerge/quizfeuerwehrauto">Feuerwehrauto-Quiz</a>
-    <a href="/LoeschZwerge/quizallgemein">Allgemein-Quiz</a>
+    <a v-for="quiz in quizzes" :href="quiz.href">
+      <div class="quiz-content">
+        <div>
+          <div>{{ quiz.title }}</div>
+          <div style="text-align: center">Quiz</div>
+        </div>
+        <div class="quiz-image">
+          <img :src="quiz.image" />
+        </div>
+      </div>
+    </a>
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  name: "Quiz",
+  data() {
+    return {
+      quizzes: [],
+    };
+  },
+  methods: {
+    async getQuizzes() {
+      const module = await import(`../files/quiz/all_quizzes.js`);
+      console.log(module);
+      this.quizzes = module.importedQuizzes;
+    },
+  },
+  mounted() {
+    this.getQuizzes();
+  },
+};
+</script>
 
 <style scoped>
 .quiz-container {
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  flex-wrap: wrap;
+  flex-direction: flex-start;
   justify-content: center;
-  height: 100vh;
+  margin: 1vw;
+  padding: 1vw;
+  gap: 1vw;
+  position: relative;
+  top: 8vw;
 }
 
 a {
-  display: block;
+  width: 40vw;
+  height: 20vw;
   margin: 10px 0;
-  padding: 10px;
-  background-color: #007bff;
-  color: white;
+  padding-left: 40px;
+  background-color: #a7a7a7;
+  color: rgb(0, 0, 0);
   text-decoration: none;
   border-radius: 5px;
-  text-align: center;
+  text-align: left;
+  align-content: center;
+  font-size: 3vw;
+  font-weight: bold;
+  font-family:
+    Tiny5,
+    Pixelify Sans,
+    sans-serif;
+}
+
+a img {
+  width: 15vw;
+}
+
+a:hover {
+  background-color: #a6e1d7;
+  color: rgb(0, 0, 0);
+  transform: scale(1.02);
+}
+
+.quiz-content {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
---
+++ FrontEnd/src/files/quiz/all_quizzes.js
@@ -0,0 +1,24 @@
+export const importedQuizzes = [
+  {
+    id: 1,
+    href: "/LoeschZwerge/quizbrandschutz",
+    title: "Brandschutz",
+    description: "Teste dein Wissen über Brandschutz und Sicherheit!",
+    image: "images/Brandschutzquiz.png",
+  },
+
+  {
+    id: 2,
+    href: "/LoeschZwerge/quizfeuerwehrauto",
+    title: "Feuerwehrauto",
+    description: "Lerne die Teile eines Feuerwehrautos kennen!",
+    image: "images/Feuerwehrautoquiz.png",
+  },
+
+  {
+    id: 3,
+    href: "/LoeschZwerge/quizallgemein",
+    title: "Allgemeinwissen",
+    image: "images/Allgemeinquiz.png",
+  },
+];
