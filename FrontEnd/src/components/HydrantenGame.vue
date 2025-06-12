de380cdd3a662fd41d2c69e68680e93f80d4a602
Erwin Holzhauser <erwinholzhauser.eh@gmail.com>
2025-06-12 15:56:16 +0200

    connected HydrantenschilKomponente mit HydrantenGame.vue

---
+++ FrontEnd/src/components/HydrantenGame.vue
@@ -0,0 +1,34 @@
+<template>
+  <HydrantenschildKomponente
+    @sendValues="getValues"
+  ></HydrantenschildKomponente>
+  <div class="hydranten-game" style="color: white">
+    <h1>Hydranten Game</h1>
+    <p>Values received: {{ values }}</p>
+  </div>
+</template>
+
+<script>
+import HydrantenschildKomponente from "./HydrantenschildKomponente.vue";
+
+export default {
+  name: "HydrantenGame",
+  components: {
+    HydrantenschildKomponente,
+  },
+  data() {
+    return {
+      values: [],
+    };
+  },
+  methods: {
+    getValues(values) {
+      this.values = values;
+      console.log("Received values:", values);
+    },
+  },
+  mounted() {
+    // Code to run when the component is mounted
+  },
+};
+</script>
--- FrontEnd/src/components/HydrantenschildKomponente.vue
+++ FrontEnd/src/components/HydrantenschildKomponente.vue
@@ -7,9 +7,10 @@
     />
     <input
       v-for="(hotspot, index) in hotspots"
+      v-model="hotspot.label"
       :key="index"
       type="text"
-      :placeholder="hotspot.label"
+      inputmode="numeric"
       :style="{
         position: 'absolute',
         top: hotspot.top,
@@ -19,150 +20,194 @@
         fontSize: hotspot.fontSize || '4vw',
       }"
       class="hotspot-input"
+      @input="createValues(index)"
+      @focus="clearOnFocus(index)"
+      maxlength="1"
     />
+  </div>
+  <div style="color: white">{{ hotspots[0].label }}</div>
+  <div style="color: white">
+    {{ values.nenndurchmesser }} {{ values.richtungLinks }}
+    {{ values.richtungRechts }} {{ values.richtungUnten }}
   </div>
 </template>
 
 <script>
 export default {
   name: "HydrantenschildKomponente",
+  emits: ["sendValues"],
   data() {
     return {
       hotspots: [
         {
-          label: "1",
+          label: "0",
           top: "24px",
           left: "109px",
           width: "31px",
           height: "48px",
+          fontSize: "55px",
         },
         {
-          label: "0",
+          label: "1",
           top: "24px",
           left: "145px",
           width: "31px",
           height: "48px",
+          fontSize: "55px",
         },
         {
-          label: "0",
+          label: "2",
           top: "24px",
           left: "181px",
           width: "31px",
           height: "48px",
-        },
-        {
-          label: "1",
-          top: "94px",
-          left: "26px",
-          width: "31px",
-          height: "48px",
-        },
-        {
-          label: "2",
-          top: "94px",
-          left: "62px",
-          width: "31px",
-          height: "48px",
+          fontSize: "55px",
         },
         {
           label: "3",
           top: "94px",
-          left: "155px",
+          left: "26px",
           width: "31px",
           height: "48px",
+          fontSize: "55px",
         },
         {
           label: "4",
           top: "94px",
-          left: "191px",
+          left: "62px",
           width: "31px",
           height: "48px",
+          fontSize: "55px",
         },
         {
           label: "5",
+          top: "94px",
+          left: "155px",
+          width: "31px",
+          height: "48px",
+          fontSize: "55px",
+        },
+        {
+          label: "",
+          top: "94px",
+          left: "191px",
+          width: "31px",
+          height: "48px",
+          fontSize: "55px",
+        },
+        {
+          label: "7",
           top: "151px",
           left: "89px",
           width: "31px",
           height: "48px",
+          fontSize: "55px",
         },
         {
-          label: "6",
+          label: "8",
           top: "151px",
           left: "125px",
           width: "31px",
           height: "48px",
+          fontSize: "55px",
         },
         {
-          label: "7",
+          label: "9",
           top: "169px",
           left: "171px",
           width: "17px",
           height: "30px",
-          fontSize: "3vw",
+          fontSize: "25px",
         },
         {
-          label: "8",
+          label: "10",
           top: "112px",
           left: "109px",
           width: "17px",
           height: "30px",
-          fontSize: "3vw",
+          fontSize: "25px",
         },
         {
-          label: "9",
+          label: "11",
           top: "112px",
           left: "236px",
           width: "17px",
           height: "30px",
-          fontSize: "3vw",
-        },
-        {
-          label: "10",
-          top: "23px",
-          left: "218px",
-          width: "3px",
-          height: "14px",
-          fontSize: "1vw",
-        },
-        {
-          label: "11",
-          top: "23px",
-          left: "226px",
-          width: "3px",
-          height: "14px",
-          fontSize: "1vw",
+          fontSize: "25px",
         },
         {
           label: "12",
           top: "23px",
-          left: "234px",
+          left: "218px",
           width: "3px",
           height: "14px",
-          fontSize: "1vw",
+          fontSize: "10px",
         },
         {
           label: "13",
           top: "23px",
-          left: "242px",
+          left: "226px",
           width: "3px",
           height: "14px",
-          fontSize: "1vw",
+          fontSize: "10px",
         },
         {
           label: "14",
           top: "23px",
+          left: "234px",
+          width: "3px",
+          height: "14px",
+          fontSize: "10px",
+        },
+        {
+          label: "15",
+          top: "23px",
+          left: "242px",
+          width: "3px",
+          height: "14px",
+          fontSize: "10px",
+        },
+        {
+          label: "16",
+          top: "23px",
           left: " 250px",
           width: "3px",
           height: "14px",
-          fontSize: "1vw",
+          fontSize: "10px",
         },
       ],
+      values: [],
     };
   },
-  methods: {},
-  mounted() {
-    // Any initialization logic can go here
+  methods: {
+    createValues(index) {
+      if (/^[0-9]$/.test(this.hotspots[index].label)) {
+        this.values.nenndurchmesser = Number(
+          `${this.hotspots[0].label}${
+            this.hotspots[1].label
+          }${this.hotspots[2].label}`,
+        );
+        const richtungLinks = `${this.hotspots[3].label}${this.hotspots[4].label}.${this.hotspots[10].label}`;
+        this.values.richtungLinks = Number(richtungLinks);
+        const richtungRechts = `${this.hotspots[5].label}${this.hotspots[6].label}.${this.hotspots[11].label}`;
+        this.values.richtungRechts = Number(richtungRechts);
+        const richtungUnten = `${this.hotspots[7].label}${this.hotspots[8].label}.${this.hotspots[9].label}`;
+        this.values.richtungUnten = Number(richtungUnten);
+
+        this.$emit("sendValues", this.values);
+      } else {
+        alert("Bitte nur Ziffern eingeben!");
+        this.hotspots[index].label = ""; // Reset to default value
+      }
+    },
+    clearOnFocus(index) {
+      this.hotspots[index].label = ""; // Clear input on focus
+    },
   },
+  mounted() {},
+  created() {},
+
+  computed: {},
 };
 </script>
 
