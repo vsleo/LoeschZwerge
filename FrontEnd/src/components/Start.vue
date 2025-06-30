cbf3920cadc72699aded859e531e6a5a68eca9fe
vsleo <leonardvs1511@gmail.com>
2025-06-30 23:02:12 +0200

    commit

--- FrontEnd/src/components/Start.vue
+++ FrontEnd/src/components/Start.vue
@@ -1,86 +1,197 @@
 <template>
+    
   <div class="body">
-    <div class="heading" style="text-align: center">
+    <div class="top-slider" ref="slider">
+      <div class="slider-track" ref="track">
+        <img
+          v-for="(img, index) in images"
+          :key="index"
+          :src="img"
+          draggable="false"
+        />
+        <img
+          v-for="(img, index) in images"
+          :key="'clone-' + index"
+          :src="img"
+          draggable="false"
+        />
+      </div>
+    </div>
+
+    <div class="heading">
       <h1>Willkommen bei den Löschzwergen!</h1>
     </div>
-    <div class="wrapper">
-      <div>
-        <p class="paragraph" style="text-align: center" ;>
-          Was genau ist die Feuerwehr? Und was macht sie überhaupt? Bei den
-          Löschzwergen lernst du mit Spiel und Spaß alles rund ums
-          Feuerwehrauto!
+
+    <!-- <div class="wrapper center">
+      <p class="paragraph">
+        Was genau ist die Feuerwehr? Und was macht sie überhaupt?<br />
+        Bei den Löschzwergen lernst du mit Spiel und Spaß alles rund ums Feuerwehrauto!
+      </p>
+    </div> -->
+
+    <div class="wrapper content-section">
+      <div class="box">
+        <h2 class="box-heading">Feuerwehrauto</h2>
+        <p>
+          Weißt du schon, was alles auf einem Einsatzfahrzeug verladen ist? Mit dem <a href="/LoeschZwerge/car">interaktiven Feuerwehrauto</a> kannst du dir einen Einblick verschaffen.
+        </p>
+      </div>
+
+      <div class="img-wrapper">
+        <a href="/LoeschZwerge/car">
+          <img class="image" src="images/lf10-front-sq.png">
+        </a>
+      </div>
+    </div>
+
+    <div class="wrapper content-section">
+
+      <div class="img-wrapper">
+        <a href="/LoeschZwerge/quiz">
+          <img class="image" src="images/lz-quiz-icon.png">
+        </a>
+      </div>
+
+      <div class="box">
+        <h3 class="box-heading">Quiz</h3>
+        <p>
+          Hier kannst du dein Wissen auf die Probe stellen! Bei unseren <a href="/LoeschZwerge/quiz">Quizzes</a> werden interessante Fragen zur Feuerwehr gestellt. Kannst du sie richtig beantworten?
         </p>
       </div>
     </div>
-    <div class="wrapper">
+
+    <div class="wrapper content-section">
+
       <div class="box">
-        <h1 class="heading" style="color: lightcoral">Die Feuerwehr</h1>
-        <p class="paragraph">
-          Die Feuerwehr hat einige wichtige Aufgaben zu erfüllen. Dazu gehören
-          Brandschutz, technische Hilfeleistung und Katastrophenschutz - Dabei
-          steht Menschenrettung immer an erster Stelle.
+        <h4 class="box-heading">Brandschutz</h4>
+        <p>
+          Spannende Videos zum Thema Feuerwehr findest du <a href="/LoeschZwerge/brandschutz">hier</a>.
         </p>
       </div>
-      <div>
-        <div class="image-slider">
-          <img src="images/slider-images/img-1.jpg" class="slide active" />
-          <img src="images/slider-images/img-2.jpg" class="slide" />
-          <img src="images/slider-images/img-3.jpg" class="slide" />
-        </div>
+
+      <div class="img-wrapper">
+        <a href="/LoeschZwerge/brandschutz">
+          <img class="image" src="images/lz-videos-icon.png">
+        </a>
       </div>
+
     </div>
   </div>
 </template>
 
+
 <script setup>
-import { onMounted } from "vue";
+import { ref, onMounted } from 'vue'
+
+const images = [
+  'images/slider-images/img-1.jpg',
+  'images/slider-images/img-2.jpg',
+  'images/slider-images/img-3.jpg',
+  'images/slider-images/img-4.jpg',
+  'images/slider-images/img-5.jpg',
+  'images/slider-images/img-6.jpg',
+  'images/slider-images/img-7.jpg',
+  'images/slider-images/img-8.jpeg',
+  'images/slider-images/img-9.jpg',
+]
+
+const track = ref(null)
+const slider = ref(null)
 
 onMounted(() => {
-  const slides = document.querySelectorAll(".slide");
-  let currentIndex = 0;
+  let offset = 0
+  const speed = 0.23
+  const trackEl = track.value
 
-  function showNextSlide() {
-    console.log("Switching slides");
-    slides[currentIndex].classList.remove("active");
-    currentIndex = (currentIndex + 1) % slides.length;
-    slides[currentIndex].classList.add("active");
+  function animate() {
+    offset -= speed
+    if (Math.abs(offset) >= trackEl.scrollWidth / 2) {
+      offset = 0
+    }
+    trackEl.style.transform = `translateX(${offset}px)`
+    requestAnimationFrame(animate)
   }
 
-  setInterval(showNextSlide, 3000);
-});
+  animate()
+})
 </script>
 
+
+
 <style scoped>
+
+p {
+  font-size: 1.2rem;
+  line-height: 1.6;
+}
+
+a {
+  color: rgb(128, 149, 243);
+  text-decoration: none;
+  transition: color 0.3s ease-out;
+}
+
+a:hover {
+  color: rgb(179, 197, 255);
+  transition: color 0.3s ease;
+}
+
 .body {
-  position: relative;
-  top: 7vw;
+  padding-top: 5vw;
+  display: flex;
+  flex-direction: column;
+  gap: 3vw;
+  color: white;
+  font-family: Consolas, monospace;
+  padding-bottom: 7vw;
+}
+
+.heading h1 {
+  color: white;
+  text-align: center;
+  font-size: 4rem;
 }
 
 .wrapper {
   display: flex;
-  margin-left: 7vw;
-  margin-right: 7vw;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding: 0 15vw;
+  gap: 3vw;
 }
 
-.heading {
-  color: white;
-  font-size: x-large;
-  font-family: Consolas, monospace;
+.content-section {
+  align-items: stretch;
 }
 
 .box {
-  flex: 1;
-  padding: 2rem;
-  margin: 0 2vw;
+  flex: 1 1 300px;
   background: linear-gradient(to top left, #161616, #414141);
-  border-radius: 12px;
-  box-shadow: 2.5px 2.5px 2.5px #414141;
+  padding: 2rem;
+  border-radius: 16px;
+  box-shadow: 0 0 7px rgba(255, 255, 255, 0.5);
 }
 
-.paragraph {
-  color: white;
-  font-size: x-large;
-  font-family: Consolas, monospace;
+.box-heading {
+  color: lightcoral;
+  font-size: 1.8rem;
+  margin-bottom: 1rem;
+}
+
+.center {
+  justify-content: center;
+  text-align: center;
+}
+
+.img-wrapper {
+  display: flex;
+  justify-content: center;
+  width: 33.5vw;
+}
+
+.image {
+  height: 300px;
+  transition: transform 0.3s ease-out;
 }
 
 .image:hover {
@@ -94,9 +205,8 @@ onMounted(() => {
   height: 230px;
   overflow: hidden;
   position: relative;
-  width: 50vw;
-  height: 20vw;
-  overflow: hidden;
+  background-color: black;
+  box-shadow: 0px 5px 10px rgba(255, 255, 255, 0.7);
 }
 
 .slider-track {
@@ -113,4 +223,5 @@ onMounted(() => {
   user-select: none;
   margin-right: 5px;
 }
+
 </style>
