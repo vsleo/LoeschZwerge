3daf003b567b0588294f5087d0637296cbaf3142
Erwin Holzhauser <erwinholzhauser.eh@gmail.com>
2025-06-30 23:40:04 +0200

    asda

--- FrontEnd/tests/start.spec.js
+++ FrontEnd/tests/start.spec.js
@@ -1,4 +1,4 @@
-const { test, expect } = require("@playwright/test");
+import { test, expect } from "@playwright/test";
 
 test.describe("Löschzwerge Seite", () => {
   test("Seite lädt und zeigt Slider sowie Inhalte", async ({ page }) => {
